3f86254d6a24a626cc3598a34a9737c53c43ee62
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-02-12 16:35:55 +0700

    Functional Tools

--- Chapter 2/The Not-So-Basics.py
+++ Chapter 2/The Not-So-Basics.py
@@ -139,3 +139,44 @@ s.add(4)
 print(s.contains(4))        # True
 s.remove(3)
 print(s.contains(3))        # False
+
+# Functional Tools
+def exp(base, power):
+    return base ** power
+
+def two_to_the(power):
+    return exp(2, power)
+
+# better way
+from functools import partial
+two_to_the = partial(exp, 2)
+print(two_to_the(3))        # equals 8
+
+square_of = partial(exp, power=2)
+print(square_of(3))         # equals 9
+
+def double(x):
+    return 2 * x
+
+xs = [1, 2, 3, 4]
+twice_xs = [double(x) for x in xs]      # [2, 4, 6, 8]
+twice_xs = map(double, xs)              # same as above
+list_doubler = partial(map, double)     # *function* that doubles a list
+twice_xs = list_doubler(xs)             # again [2, 4, 6, 8]
+
+def multiply(x, y): return x * y
+
+products = map(mulitply, [1, 2], [4, 5])        # [1 * 4, 2 * 5] = [4, 10]
+
+def is_even(x):
+    '''True if x is even, False if x is odd'''
+    return x % 2 == 0
+
+x_evens = [x for x in xs if is_even(x)]         # [2, 4]
+x_evens = filter(is_even, xs)                   # same as above
+list_evener = partial(filter, is_even)          # *function* that filters a list
+x_evens = list_evener(xs)                       # again [2, 4]
+
+x_product = reduce(mulitply, xs)                # = 1 * 2 * 3 * 4 = 24
+list_product = partial(reduce, mulitply)        # *function* that reduces a list
+x_product = list_product(xs)                    # again 24
